b13b8c882f7cddcd0e9873d257248e2e31fe5970
Edson Ifarraguirre Moreno <10083346@facin.portoalegre.pucrsnet.br>
2025-08-28 11:23:49 -0300

    Fechamento da implementação de pilha e implementação inicial de fila

---
+++ Aula_0828/Pilha/src/Pilha.java
@@ -0,0 +1,73 @@
+// Implementação de um pilha para armazenamento de inteiros
+
+public class Pilha{
+
+    private int [] arranjo;
+    private int topo;
+
+    public Pilha(){
+        this.arranjo = new int[10];
+        this.topo=0;
+    }
+
+    public Pilha(int capacidade){
+        this.arranjo = new int[capacidade];
+        this.topo=0;
+    }
+
+    //insere o elemento e no topo da pilha
+    public boolean push(int e){
+        if(topo>=arranjo.length)
+            return false;
+
+        arranjo[topo]=e;
+        topo++;
+        return true;
+    }
+    //remove e retorna o elemento do topo da pilha 
+    //  (erro se a pilha estiver vazia)
+    public int pop(){
+        if(topo>0){
+            topo--;
+            return arranjo[topo];
+        }
+        throw new RuntimeException("Stack underflow. A Pilha está vazia");
+    }
+
+    //retorna, mas não remove, o elemento do topo da pilha 
+    // (erro se a pilha estiver vazia)
+    public int top(){        
+        // se a pilha estiver vazia
+        if(topo==0)
+            throw new RuntimeException("A pilha está vazia");        
+        return arranjo[topo-1];
+    }
+
+    //retorna o número de elementos da pilha
+    public int size(){
+        return topo;
+    }
+
+    //retorna true se a pilha estiver vazia, e false caso contrário
+    public boolean isEmpty(){        
+        return (topo==0);
+    }
+
+    //esvazia a pilha
+    public void clear(){        
+        topo=0;
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("0->[");
+        for(int i=0; i<topo; i++){
+            sb.append(arranjo[i]);
+            if(i!=topo-1)
+                sb.append(",");
+        }
+        sb.append("]<-"+topo+"..."+arranjo.length);
+        sb.append("\n");
+        return sb.toString();
+    }
+}
---
+++ Aula_0828/Pilha/src/Principal.java
@@ -0,0 +1,43 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        Pilha p = new Pilha(4);
+
+        p.push(7);
+        System.out.println(p);
+
+        p.push(3);
+        System.out.println(p);
+        
+        p.push(1);
+        System.out.println(p);
+        
+        p.push(0);
+        System.out.println(p);
+        
+        p.push(37);
+        System.out.println(p);
+
+        System.out.println("Removendo da pilha: "+p.pop());
+        System.out.println(p);
+        
+        System.out.println("Removendo da pilha: "+p.pop());
+        System.out.println(p);
+        
+        System.out.println("Removendo da pilha: "+p.pop());
+        System.out.println(p);
+        
+        System.out.println("Removendo da pilha: "+p.pop());
+        System.out.println(p);
+        
+        System.out.println("Removendo da pilha: "+p.pop());
+        System.out.println(p);
+        
+        System.out.println("Removendo da pilha: "+p.pop());
+        System.out.println(p);
+        
+        System.out.println("Removendo da pilha: "+p.pop());
+        System.out.println(p);
+        
+    }
+}

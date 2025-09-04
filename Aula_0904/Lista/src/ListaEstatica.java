c5212c8c9024a9e713c81de735d26daf922bfdb1
Edson Ifarraguirre Moreno <10083346@facin.portoalegre.pucrsnet.br>
2025-09-04 10:32:30 -0300

    Inclusão do arquivo base para implementação da lista

---
+++ Aula_0904/Lista/src/ListaEstatica.java
@@ -0,0 +1,26 @@
+
+public class ListaEstatica {
+
+    public boolean add(int e){
+    }
+    //public void add(int index, int element){}
+    //public boolean addAll(int[] c) 
+    //public boolean addAll(int index, int[] c) 
+    public void clear(){
+    }
+    //public boolean contains(Object o) 
+    public int get(int index){
+    }
+    //public int indexOf(int o) 
+    public boolean isEmpty(){
+    }
+    //public int lastIndexOf(int o) 
+    public int remove(int index){
+    }
+    //public boolean remove(int o) 
+    //public boolean removeAll(int[] c) 
+    //public int set(int index, int element) 
+    public int size(){
+    }
+    //public int[] subList(int fromIndex, int toIndex) 
+}
